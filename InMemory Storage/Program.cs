ad4b1571c585b81a8ee3ae3e9b4ed06579b848e6
Talha Malik <48487598+TalhaMaalik@users.noreply.github.com>
2024-07-06 21:14:38 +0100

    Boilerplate code setup

--- InMemory Storage/Program.cs
+++ InMemory Storage/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InMemory_Storage.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace InMemory_Storage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
---
+++ InMemory Storage/Services/Worker.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InMemory_Storage.Services
+{
+    public class Worker : BackgroundService
+    {
+        public Worker(ILogger<Worker> logger)
+        {
+            Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        private readonly ILogger<Worker> Logger;
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            return Task.CompletedTask;
+        }
+    }
+}
