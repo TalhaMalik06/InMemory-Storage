0f4c8792c0b5369db89933fd6c5d85bd0825e6fe
TalhaMalik06 <mrtalhamalik97@gmail.com>
2024-07-10 22:44:15 +0100

    Added get command handler

---
+++ InMemory Storage/Commands/GetItemCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InMemory_Storage.Commands
+{
+    public class GetItemCommandHandler : ICommandHandler
+    {
+        public bool CanHandle(string commandType)
+        {
+            return commandType.Equals("GET", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public Task<string> Handle(string command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
